2e0ae7a491bf73e9c67723b4b5ae7566f879c526
Smotrin_ME <smotrin_me@sptnk.co>
2017-10-10 12:40:35 +0400

    multi thread per thread on client

--- demoMaven/src/main/java/main/Main.java
+++ demoMaven/src/main/java/main/Main.java
@@ -11,6 +11,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import resourceServer.ResourceServerControllerMBean;
 import resourceServer.ResourceServerController;
 import resources.TestResource;
+import server.ThreadHandler;
 import servlets.ResourceServlet;
 
 import javax.management.MBeanServer;
@@ -23,30 +24,29 @@ import java.net.Socket;
 /**
  * Created by misha on 06.12.16.
  */
-public class Main {
+public class Main extends Thread{
     // Выбираем порт вне пределов 1-1024:
-    public static final int PORT = 8080;
+    public static final int PORT = 5050;
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket s = new ServerSocket(PORT)){
-            System.out.println("Started: " + s);
-          // Блокирует до тех пор, пока не возникнет соединение:
-        try (Socket socket = s.accept()){
-                System.out.println("Connection accepted: " + socket);
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        socket.getInputStream()));
-                // Вывод автоматически выталкивается из буфера PrintWriter'ом
-                PrintWriter out = new PrintWriter(new BufferedWriter(
-                        new OutputStreamWriter(socket.getOutputStream())), true);
-                while (true) {
-                    String str = in.readLine();
-                    if (str.equals("END"))
-                        break;
-                    System.out.println("Echoing: " + str);
-                    out.println(str);
+        ServerSocket s = new ServerSocket(PORT);
+        System.out.println("Server started");
+        try {
+            while (true) {
+                // Блокируется до возникновения нового соединения:
+                Socket socket = s.accept();
+                try {
+                    new ThreadHandler(socket);
                 }
-                // Всегда закрываем два сокета...
+                catch (IOException e) {
+                    // Если завершится неудачей, закрывается сокет,
+                    // в противном случае, нить закроет его:
+                    socket.close();
+                }
             }
+        }
+        finally {
+            s.close();
         }
     }
 }
--- demoMaven/src/main/java/server/ThreadHandler.java
+++ demoMaven/src/main/java/server/ThreadHandler.java
@@ -1,19 +1,50 @@
 package server;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
  * Created by misha on 10.10.17.
  */
-public class ThreadHandler implements Runnable {
-
+public class ThreadHandler extends Thread  {
     private Socket socket;
-    public ThreadHandler(Socket socket){
-        this.socket = socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ThreadHandler(Socket s) throws IOException {
+        socket = s;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // Включаем автоматическое выталкивание:
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                .getOutputStream())), true);
+        // Если любой из вышеприведенных вызовов приведет к
+        // возникновению исключения, то вызывающий отвечает за
+        // закрытие сокета. В противном случае, нить
+        // закроет его.
+        start(); // вызываем run()
     }
 
-    @Override
     public void run() {
-
+        try {
+            while (true) {
+                String str = in.readLine();
+                if (str.equals("Bue."))
+                    break;
+                System.out.println("Echoing: " + str);
+                out.println(str);
+            }
+            System.out.println("closing...");
+        }
+        catch (IOException e) {
+            System.err.println("IO Exception");
+        }
+        finally {
+            try {
+                socket.close();
+            }
+            catch (IOException e) {
+                System.err.println("Socket not closed");
+            }
+        }
     }
 }
